016dd05dae397db55b80c408ebd5265833c10241
Manlo Ding <36906371+mandaloo@users.noreply.github.com>
2021-06-30 13:40:24 +0000

    less

---
+++ src/components/Hello/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './index.less';
+
+interface Props {}
+
+const Hello = (props: Props) => {
+  return <div className={styles.text}>Hello</div>;
+};
+
+export default Hello;
+``
